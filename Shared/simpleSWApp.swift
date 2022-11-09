dea30d6aa8e3a98607f723f0efc983e95d5f4e79
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2022-11-09 21:43:16 +0900

    Initial Commit

---
+++ Shared/simpleSWApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleSWApp.swift
+//  Shared
+//
+//  Created by 林宏樹 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct simpleSWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
